420877faf2896f7b0724bfea3b12bb4d12f92848
lunrjx <lucasjrebello@gmail.com>
2025-05-20 16:07:11 -0500

    B: add funcs

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = countem(text)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def countem(text):
+    words = text.split()
+    return len(words)
+
+main()
+
+
+
+
